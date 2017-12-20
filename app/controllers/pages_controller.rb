2312fcd1a3c825e05f48e53cc215110c0c0174e1
James Doc <jamesdoc@gmail.com>
2017-12-20 12:26:33 +0000

    Switch asset_url for asset_path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,12 +30,12 @@ private
       {
         "title" => 'About',
         "image" => {
-          "320": view_context.asset_url("static-banner/church_320.jpg"),
-          "640": view_context.asset_url("static-banner/church_640.jpg"),
-          "960": view_context.asset_url("static-banner/church_960.jpg"),
-          "1280": view_context.asset_url("static-banner/church_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/church_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/church_2560.jpg"),
+          "320": view_context.asset_path("static-banner/church_320.jpg"),
+          "640": view_context.asset_path("static-banner/church_640.jpg"),
+          "960": view_context.asset_path("static-banner/church_960.jpg"),
+          "1280": view_context.asset_path("static-banner/church_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/church_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/church_2560.jpg"),
         },
       }
     when 'about/doctrinal-basis'
@@ -48,24 +48,24 @@ private
       {
         "title" => 'What we do',
         "image" => {
-          "320": view_context.asset_url("static-banner/about_320.jpg"),
-          "640": view_context.asset_url("static-banner/about_640.jpg"),
-          "960": view_context.asset_url("static-banner/about_960.jpg"),
-          "1280": view_context.asset_url("static-banner/about_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/about_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/about_2560.jpg"),
+          "320": view_context.asset_path("static-banner/about_320.jpg"),
+          "640": view_context.asset_path("static-banner/about_640.jpg"),
+          "960": view_context.asset_path("static-banner/about_960.jpg"),
+          "1280": view_context.asset_path("static-banner/about_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/about_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/about_2560.jpg"),
         },
       }
     when 'get-involved'
       {
         "title" => 'Get involved',
         "image" => {
-          "320": view_context.asset_url("static-banner/the-globe-church-training_320.jpg"),
-          "640": view_context.asset_url("static-banner/the-globe-church-training_640.jpg"),
-          "960": view_context.asset_url("static-banner/the-globe-church-training_960.jpg"),
-          "1280": view_context.asset_url("static-banner/the-globe-church-training_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/the-globe-church-training_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/the-globe-church-training_2560.jpg"),
+          "320": view_context.asset_path("static-banner/the-globe-church-training_320.jpg"),
+          "640": view_context.asset_path("static-banner/the-globe-church-training_640.jpg"),
+          "960": view_context.asset_path("static-banner/the-globe-church-training_960.jpg"),
+          "1280": view_context.asset_path("static-banner/the-globe-church-training_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/the-globe-church-training_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/the-globe-church-training_2560.jpg"),
         },
       }
     when 'contact'
@@ -73,12 +73,12 @@ private
         "title" => 'Get in touch',
         "subtitle" => 'Got a question? New in the area? Just want to find out more? We\'d love talk.',
         "image" => {
-          "320": view_context.asset_url("static-banner/contact/contact_320.jpg"),
-          "640": view_context.asset_url("static-banner/contact/contact_640.jpg"),
-          "960": view_context.asset_url("static-banner/contact/contact_960.jpg"),
-          "1280": view_context.asset_url("static-banner/contact/contact_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/contact/contact_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/contact/contact_2560.jpg"),
+          "320": view_context.asset_path("static-banner/contact/contact_320.jpg"),
+          "640": view_context.asset_path("static-banner/contact/contact_640.jpg"),
+          "960": view_context.asset_path("static-banner/contact/contact_960.jpg"),
+          "1280": view_context.asset_path("static-banner/contact/contact_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/contact/contact_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/contact/contact_2560.jpg"),
         },
       }
     when 'christmas'
@@ -86,12 +86,12 @@ private
         "title" => 'Christmas 2017',
         "subtitle" => 'Carol services near London Bridge and Waterloo',
         "image" => {
-          "320": view_context.asset_url("static-banner/christmas/christmas_320.jpg"),
-          "640": view_context.asset_url("static-banner/christmas/christmas_640.jpg"),
-          "960": view_context.asset_url("static-banner/christmas/christmas_960.jpg"),
-          "1280": view_context.asset_url("static-banner/christmas/christmas_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/christmas/christmas_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/christmas/christmas_2560.jpg"),
+          "320": view_context.asset_path("static-banner/christmas/christmas_320.jpg"),
+          "640": view_context.asset_path("static-banner/christmas/christmas_640.jpg"),
+          "960": view_context.asset_path("static-banner/christmas/christmas_960.jpg"),
+          "1280": view_context.asset_path("static-banner/christmas/christmas_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/christmas/christmas_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/christmas/christmas_2560.jpg"),
         },
       }
     when 'wonder'
@@ -99,12 +99,12 @@ private
         "title" => 'Have You Ever Wondered?',
         "subtitle" => 'When we were kids, there was one thing we used to be good at: WONDER',
         "image" => {
-          "320": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_320.jpg"),
-          "640": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_640.jpg"),
-          "960": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_960.jpg"),
-          "1280": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_1280.jpg"),
-          "1920": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_1920.jpg"),
-          "2560": view_context.asset_url("static-banner/wonder/have-you-ever-wondered_2560.jpg"),
+          "320": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_320.jpg"),
+          "640": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_640.jpg"),
+          "960": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_960.jpg"),
+          "1280": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_1280.jpg"),
+          "1920": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_1920.jpg"),
+          "2560": view_context.asset_path("static-banner/wonder/have-you-ever-wondered_2560.jpg"),
         },
       }
     else
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -7,12 +7,12 @@ class ResourcesController < ApplicationController
     @banner = {
       "title" => 'Preaching',
       "image" => {
-        "320": view_context.asset_url("#{img_location}320.jpg"),
-        "640": view_context.asset_url("#{img_location}640.jpg"),
-        "960": view_context.asset_url("#{img_location}960.jpg"),
-        "1280": view_context.asset_url("#{img_location}1280.jpg"),
-        "1920": view_context.asset_url("#{img_location}1920.jpg"),
-        "2560": view_context.asset_url("#{img_location}2560.jpg"),
+        "320": view_context.asset_path("#{img_location}320.jpg"),
+        "640": view_context.asset_path("#{img_location}640.jpg"),
+        "960": view_context.asset_path("#{img_location}960.jpg"),
+        "1280": view_context.asset_path("#{img_location}1280.jpg"),
+        "1920": view_context.asset_path("#{img_location}1920.jpg"),
+        "2560": view_context.asset_path("#{img_location}2560.jpg"),
       },
     }
     @latest_sermon = latest_sermon.decorate if latest_sermon
@@ -33,12 +33,12 @@ class ResourcesController < ApplicationController
       "title" => "Blog",
       "size" => "small",
       "image" => {
-        # "320": view_context.asset_url("#{img_location}320.jpg"),
-        "640": view_context.asset_url("#{img_location}640.jpg"),
-        "960": view_context.asset_url("#{img_location}960.jpg"),
-        "1280": view_context.asset_url("#{img_location}1280.jpg"),
-        "1920": view_context.asset_url("#{img_location}1920.jpg"),
-        "2560": view_context.asset_url("#{img_location}2560.jpg"),
+        # "320": view_context.asset_path("#{img_location}320.jpg"),
+        "640": view_context.asset_path("#{img_location}640.jpg"),
+        "960": view_context.asset_path("#{img_location}960.jpg"),
+        "1280": view_context.asset_path("#{img_location}1280.jpg"),
+        "1920": view_context.asset_path("#{img_location}1920.jpg"),
+        "2560": view_context.asset_path("#{img_location}2560.jpg"),
       },
     }
   end
