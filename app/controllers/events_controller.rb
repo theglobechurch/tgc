086dbb09ea9d4915b9cc91a44023546ea273966b
James Doc <jamesdoc@gmail.com>
2019-03-07 10:53:06 +0000

    Show all events in the calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,16 +2,40 @@ class EventsController < ApplicationController
   layout 'application'
 
   def index
-    @events = events
-    @today = Time.now
+    img_location = 'static-banner/the-globe-church-preaching_'
+    @banner = {
+      "title" => 'Church life',
+      "subtitle" => 'Join us at one of the upcoming events at The Globe Church',
+      "image" => {
+        "320": view_context.asset_path("#{img_location}320.jpg"),
+        "640": view_context.asset_path("#{img_location}640.jpg"),
+        "960": view_context.asset_path("#{img_location}960.jpg"),
+        "1280": view_context.asset_path("#{img_location}1280.jpg"),
+        "1920": view_context.asset_path("#{img_location}1920.jpg"),
+        "2560": view_context.asset_path("#{img_location}2560.jpg"),
+      },
+    }
+    @this_week = this_week
+    @future_events = future_events
+
   end
 
 private
-  def events
-    @events ||= EventInstance.all
-                  .joins(:event)
-                  .where('events.state': 'published')
-                  .where("end_datetime >= ?", Date.current)
-                  .order(:start_datetime)
+  def this_week
+    @this_week ||= EventInstance.all
+                    .joins(:event)
+                    .where('events.state': 'published')
+                    .where("end_datetime >= ?", Time.now)
+                    .where("end_datetime < ?", Time.now + 1.week)
+                    .order(:start_datetime)
+  end
+
+  def future_events
+    @future_events ||= EventInstance.all
+                        .joins(:event)
+                        .where('events.state': 'published')
+                        .where("end_datetime >= ?", Time.now + 1.week)
+                        .order(:start_datetime)
+
   end
 end
