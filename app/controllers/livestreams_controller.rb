e3931c4289e829c96f912efa66bd12e2d46d6bbf
James Doc <hi@jamesdoc.com>
2020-03-19 23:22:14 +0000

    Hack the open graph img

--- app/controllers/livestreams_controller.rb
+++ app/controllers/livestreams_controller.rb
@@ -4,8 +4,11 @@ class LivestreamsController < ApplicationController
     @banner = {
       'title' => 'Live Stream',
       'size' => 'video',
+      'description' => 'Church is about gathering together as God’s people. We think that is a precious and wonderful thing. There are occasions when gathering is not possible and at those times a livestream can help us until we can gather again…',
       'video' => "https://www.youtube.com/embed/#{youtubeId}",
       'hide-text' => true,
+      'socialImg' => '/assets/live-og.jpg',
     }
+
   end
 end
