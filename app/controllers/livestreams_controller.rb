b5b17379a40dcb33d75ae141e62ee6f0be4e42ef
James Doc <hi@jamesdoc.com>
2020-03-22 10:26:24 +0000

    Pull livestreams from the database

--- app/controllers/livestreams_controller.rb
+++ app/controllers/livestreams_controller.rb
@@ -1,14 +1,30 @@
 class LivestreamsController < ApplicationController
   def index
-    youtubeId = '3AUGRNks8d4'
+    @main_stream = main_stream
+    @previous_streams = previous_streams
+
     @banner = {
       'title' => 'Live Stream',
       'size' => 'video',
       'description' => 'Church is about gathering together as God’s people. We think that is a precious and wonderful thing. There are occasions when gathering is not possible and at those times a livestream can help us until we can gather again…',
-      'video' => "https://www.youtube.com/embed/#{youtubeId}",
+      'video' => "https://www.youtube.com/embed/#{@main_stream.youtubeId}",
       'hide-text' => true,
       'socialImg' => '/assets/live-og.jpg',
     }
+  end
 
+private
+
+  def main_stream
+    @main_stream ||= Livestream.all
+                       .where("live_at >= ?", Time.now - 8.hour)
+                       .where("live_at <= ?", Time.now + 8.hour)
+                       .first
+  end
+
+  def previous_streams
+    @previous_streams ||= Livestream.all
+                            .where("live_at <= ?", Time.now)
+                            .order(:live_at)
   end
 end
