51b7359068b3f0ecb1bc933ceeabfac8a5da3014
James Doc <hi@jamesdoc.com>
2020-05-08 17:41:43 +0100

    Auto lib in production

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Globe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.enable_dependency_loading = true
+    config.autoload_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over these.
     # Application configuration should go into files in config/initializers
